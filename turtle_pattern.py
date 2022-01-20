e5f4fdc7a095d463007c714eef9d1d23430a6381
AKSHAY MISHRA <72158107+akshaymishra7@users.noreply.github.com>
2022-01-21 02:15:50 +0530

    Add files via upload

---
+++ turtle_pattern.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.bgcolor("black")
+turtle.pensize(4)
+turtle.speed(0)
+
+b=["red","blue","green","purple"]
+
+for i in range:
+    for a in b:
+        turtle.color(a)
+        turtle.circle(100)
+        turtle.left(10)
+turtle.mainloop()        
